460535faba25511a0376bd6a6b966d3878857bb4
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-29 23:19:27 +0100

    Add an ObjectProperty to retrieve the TextInput's content when the 'Search' button is clicked.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+
+    def search_location(self):
+        print "The user searched for: {}".format(self.search_input.text)
 
 
 class WeatherApp(App):
