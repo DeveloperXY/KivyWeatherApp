7eba6994aa400070106e768a82ed8589ec861277
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-30 12:42:04 +0100

    Move the WeatherRoot widget to its own separate .py file.

---
+++ weather_root.py
@@ -0,0 +1,24 @@
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+from add_location_form import AddLocationForm
+
+
+class WeatherRoot(BoxLayout):
+    current_weather = ObjectProperty()
+
+    def show_current_weather(self, location):
+        self.clear_widgets()
+
+        if location is None and self.current_weather is None:
+            location = "New York (US)"
+        if location is not None:
+            self.current_weather = Factory.CurrentWeather()
+            self.current_weather.location = location
+
+        self.add_widget(self.current_weather)
+
+    def show_add_location_form(self):
+        self.clear_widgets()
+        self.add_widget(AddLocationForm())
