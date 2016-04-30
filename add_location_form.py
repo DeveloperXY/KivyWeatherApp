3496652c79e5d9ffe5d9533bf50d66aa5f4165ab
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-30 12:40:02 +0100

    Move the AddLocationForm class to its own separate .py file.

---
+++ add_location_form.py
@@ -0,0 +1,22 @@
+from kivy.network.urlrequest import UrlRequest
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+API_KEY = '787d0e1db679f03b5812253618f1481a'
+
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+    search_results = ObjectProperty()
+
+    def search_location(self):
+        search_template = "http://api.openweathermap.org/data/2.5/find?q={}&appid={}"
+        search_url = search_template.format(self.search_input.text, API_KEY)
+        request = UrlRequest(search_url, self.found_location)
+
+    def found_location(self, request, data):
+        cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
+        # self.search_results.item_strings = cities
+        del self.search_results.adapter.data[:]
+        self.search_results.adapter.data.extend(cities)
+        self.search_results._trigger_reset_populate()
