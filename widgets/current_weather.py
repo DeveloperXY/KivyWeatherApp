e3f38b7d2b71eef10d28cab1fe5ab389441a0882
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-30 17:44:48 +0100

    CurrentWeather: Change the conditions attribute from a StringProperty to an ObjectProperty.

--- widgets/current_weather.py
+++ widgets/current_weather.py
@@ -1,13 +1,14 @@
 from kivy.network.urlrequest import UrlRequest
-from kivy.properties import ListProperty, StringProperty, NumericProperty
+from kivy.properties import ListProperty, NumericProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.factory import Factory
 
 API_KEY = '787d0e1db679f03b5812253618f1481a'
 
 
 class CurrentWeather(BoxLayout):
     location = ListProperty(['Fez', 'MA'])
-    conditions = StringProperty()
+    conditions = ObjectProperty()
     temp = NumericProperty()
     temp_min = NumericProperty()
     temp_max = NumericProperty()
@@ -19,7 +20,13 @@ class CurrentWeather(BoxLayout):
         request = UrlRequest(weather_url, self.weather_retrieved)
 
     def weather_retrieved(self, request, data):
-        self.conditions = data['weather'][0]['description']
+        self.render_conditions(data['weather'][0]['description'])
         self.temp = data['main']['temp']
         self.temp_min = data['main']['temp_min']
         self.temp_max = data['main']['temp_max']
+
+    def render_conditions(self, conditions_description):
+        conditions_widget = Factory.UnknownConditions()
+        conditions_widget.conditions = conditions_description
+        self.conditions.clear_widgets()
+        self.conditions.add_widget(conditions_widget)
