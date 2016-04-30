7d3278507fe1765109ae80ceb24dbfe5d40640d2
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-30 18:01:34 +0100

    Add the 'ClearConditions' widget.

--- widgets/current_weather.py
+++ widgets/current_weather.py
@@ -26,7 +26,9 @@ class CurrentWeather(BoxLayout):
         self.temp_max = data['main']['temp_max']
 
     def render_conditions(self, conditions_description):
-        conditions_widget = Factory.UnknownConditions()
+        conditions_widget = Factory.ClearConditions() \
+            if "clear" in conditions_description \
+            else Factory.UnknownConditions()
         conditions_widget.conditions = conditions_description
         self.conditions.clear_widgets()
         self.conditions.add_widget(conditions_widget)
