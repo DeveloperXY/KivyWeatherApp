bf93506a9eaf152508c43ac07d98af554a3c0947
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-30 12:34:12 +0100

    Move the AddLocationForm to its own separate layout file.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from kivy.app import App
+from kivy.factory import Factory
 from kivy.network.urlrequest import UrlRequest
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton
-from kivy.factory import Factory
 
 API_KEY = '787d0e1db679f03b5812253618f1481a'
 
 
 class WeatherRoot(BoxLayout):
+    current_weather = ObjectProperty()
+
     def show_current_weather(self, location):
         self.clear_widgets()
-        current_weather = Factory.CurrentWeather()
-        current_weather.location = location
-        self.add_widget(current_weather)
+
+        if location is None and self.current_weather is None:
+            location = "New York (US)"
+        if location is not None:
+            self.current_weather = Factory.CurrentWeather()
+            self.current_weather.location = location
+
+        self.add_widget(self.current_weather)
 
     def show_add_location_form(self):
         self.clear_widgets()
