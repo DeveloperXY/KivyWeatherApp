0c8374f3c73b72fe4d9f5683ff32b8a84c1aca72
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-01 19:08:06 +0100

    Store locations using Kivy's JsonStore.

--- widgets/weather_root.py
+++ widgets/weather_root.py
@@ -1,5 +1,6 @@
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
+from kivy.storage.jsonstore import JsonStore
 from kivy.uix.boxlayout import BoxLayout
 
 from add_location_form import AddLocationForm
@@ -10,6 +11,14 @@ class WeatherRoot(BoxLayout):
     current_weather = ObjectProperty()
     locations = ObjectProperty()
 
+    def __init__(self, **kwargs):
+        super(WeatherRoot, self).__init__(**kwargs)
+        self.store = JsonStore("weather_store.json")
+
+        if self.store.exists('locations'):
+            current_location = self.store.get("locations")["current_location"]
+            self.show_current_weather(current_location)
+
     def show_current_weather(self, location=None):
         self.clear_widgets()
 
@@ -17,12 +26,19 @@ class WeatherRoot(BoxLayout):
             self.current_weather = CurrentWeather()
         if self.locations is None:
             self.locations = Factory.Locations()
+            if self.store.exists('locations'):
+                locations = self.store.get("locations")['locations']
+                self.locations.locations_list.adapter.data.extend(locations)
         if location is not None:
             self.current_weather = CurrentWeather(location=location)
 
-            if location not in self.locations.locations_list.adapter.data:
-                self.locations.locations_list.adapter.data.append(location)
+            adapter_data = self.locations.locations_list.adapter.data
+
+            if location not in adapter_data:
+                adapter_data.append(location)
                 self.locations.locations_list._trigger_reset_populate()
+                self.store.put("locations", locations=list(adapter_data),
+                               current_location=location)
 
         self.current_weather.update_weather()
         self.add_widget(self.current_weather)
