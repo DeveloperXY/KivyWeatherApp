2076ddcae4bc3945554e962d3f4880b5ca312e57
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-01 19:28:30 +0100

    Create a custom "Weather Settings" panel.

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ from widgets.location_button import LocationButton
 
 
 class WeatherApp(App):
-    pass
+    def build_config(self, config):
+        config.setdefaults('General', {'temp_type': "Metric"})
+
+    def build_settings(self, settings):
+        settings.add_json_panel("Weather Settings", self.config,
+                                data="""
+                                [
+                                    {
+                                        "type": "options",
+                                        "title": "Temperature System",
+                                        "section": "General",
+                                        "key": "temp_type",
+                                        "options": ["Metric", "Imperial"]
+                                    }
+                                ]
+                                """)
 
 
 def args_converter(index, data_item):
