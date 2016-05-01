8f7bfb81978abcecef52231a92050c375608e8c3
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-01 18:55:10 +0100

    Locations: Store locations temporarily in memory.

--- widgets/weather_root.py
+++ widgets/weather_root.py
@@ -1,6 +1,6 @@
+from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.factory import Factory
 
 from add_location_form import AddLocationForm
 from widgets.current_weather import CurrentWeather
@@ -19,6 +19,10 @@ class WeatherRoot(BoxLayout):
             self.locations = Factory.Locations()
         if location is not None:
             self.current_weather = CurrentWeather(location=location)
+
+            if location not in self.locations.locations_list.adapter.data:
+                self.locations.locations_list.adapter.data.append(location)
+                self.locations.locations_list._trigger_reset_populate()
 
         self.current_weather.update_weather()
         self.add_widget(self.current_weather)
